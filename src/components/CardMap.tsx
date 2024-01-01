21355f9605b2c821411197be8e802b3284405fd2
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2024-01-01 15:23:07 -0400

    :sparkles: feat: Component Map

---
+++ src/components/CardMap.tsx
@@ -0,0 +1,12 @@
+export default function CardMap() {
+  return (
+    <>
+      <iframe
+        id="gmap_canvas"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31873.241962388765!2d-59.962366500000016!3d-3.0530829999999907!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x926c1b95bef245b3%3A0x6bd278631b01ac0c!2sR.%20Aragarcas%20Cj%20Manoa%2C%2016%20-%20Lot%20Novo%20Aleixo%2C%20Manaus%20-%20AM%2C%2069090-772!5e0!3m2!1spt-BR!2sbr!4v1704136471271!5m2!1spt-BR!2sbr"
+        scrolling="no"
+        className="h-80 w-full"
+      />
+    </>
+  )
+}
