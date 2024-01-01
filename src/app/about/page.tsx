5d30443a80b58388dc0904d607ce8ab98f139f21
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2024-01-01 13:47:17 -0400

    :iphone: Tablet

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,22 +7,38 @@ export default function page() {
       <h1 className="mb-5 mt-2 text-3xl">Sobre</h1>
       <section
         className="flex h-3/5 w-10/12 flex-col items-center bg-[#373A3C]  
-      p-5 md:flex-row md:justify-around"
+      p-5 "
       >
-        <Image
-          src={'/assets/images/profile/foto-perfil.jpg'}
-          width={350}
-          height={250}
-          alt="Um homem de pelo menos 30 anos de idade, vestindo um de trabalho de fazendo sinal de positivo, parecendo estar  com uma alegria que contagia."
-          className="mb-4 object-cover md:mb-0 "
-        />
+        <div className="w-full md:hidden">
+          <Image
+            src={'/assets/images/profile/foto-perfil.jpg'}
+            width={350}
+            height={250}
+            alt="Um homem de pelo menos 30 anos de idade, vestindo um de trabalho de fazendo sinal de positivo, parecendo estar  com uma alegria que contagia."
+            className="mb-4 object-cover 
+          md:mb-0 md:h-[350px] md:w-full"
+          />
+        </div>
+        <div className="hidden w-full md:block">
+          <Image
+            src={'/assets/images/profile/perfil.webp'}
+            width={350}
+            height={250}
+            alt="Um homem de pelo menos 30 anos de idade, vestindo um de trabalho de fazendo sinal de positivo, parecendo estar  com uma alegria que contagia."
+            className="mb-4 object-cover 
+          md:mb-0 md:h-[350px] md:w-full"
+          />
+        </div>
         <div
           className="flex flex-col justify-center 
-            md:w-1/2 md:items-center md:justify-center "
+             md:items-center md:justify-center md:px-5"
         >
-          <h1 className="mb-2 text-3xl">{profile.name}</h1>
-          <h3 className="mb-2 text-2xl">Web Developer</h3>
-          <p className="mb-5 text-left text-base">
+          <h1 className="mb-2 text-3xl md:hidden">{profile.name}</h1>
+          <h3 className="mb-2 text-2xl md:mt-5">Web Developer</h3>
+          <p
+            className="mb-5 text-left text-base 
+          md:text-xl"
+          >
             Me chamo Lucas da Silva, atualmente trabalho como porteiro.
             <br /> <br />
             Estou no momento em transição de carreira.Tenho conhecimento em
