9f8e8e292dd5d14119dd25dcf5ec817b91f7600f
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2024-01-01 08:49:00 -0400

    :sparkles: feat: Archer "Types" props

---
+++ src/@types/index.ts
@@ -0,0 +1,12 @@
+export type PropsProject = {
+  project: {
+    id: string
+    name: string
+    description: string
+    image: string
+    stack: string
+    tecnologies: string[]
+    github: string
+    page: string
+  }
+}
