cc4cf8a7358909d88041d7209cce59a1248f6de8
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2024-01-01 08:54:26 -0400

    :sparkles: feat: Archer by data

---
+++ src/lib/data.ts
@@ -0,0 +1,434 @@
+export const skills = [
+  {
+    id: 1,
+    name: 'Javascript',
+    image: '/assets/images/tecnologias/javascript-original.svg',
+  },
+  {
+    id: 2,
+    name: 'Typescript',
+    image: '/assets/images/tecnologias/typescript-original.svg',
+  },
+  {
+    id: 3,
+    name: 'Node',
+    image: '/assets/images/tecnologias/node.png',
+  },
+  {
+    id: 4,
+    name: 'Express',
+    image: '/assets/images/tecnologias/express.png',
+  },
+  {
+    id: 5,
+    name: 'React',
+    image: '/assets/images/tecnologias/react-original.svg',
+  },
+  {
+    id: 6,
+    name: 'Tailwind',
+    image: '/assets/images/tecnologias/tailwindcss-plain.svg',
+  },
+  {
+    id: 7,
+    name: 'Styled C.',
+    image: '/assets/images/tecnologias/styled-components.png',
+  },
+  {
+    id: 8,
+    name: 'Bun',
+    image: '/assets/images/tecnologias/download.svg',
+  },
+  {
+    id: 9,
+    name: 'NextJs',
+    image: '/assets/images/tecnologias/nextjs-original.svg',
+  },
+]
+
+export const profile = {
+  name: 'Lucas Gomes',
+  bannerVideo: '/assets/videos/video.mp4',
+  description: `Desenvolvedor front-end altamente motivado e apaixonado por criar
+  interfaces de usuário atraentes e interativas. Estou no momento em
+  transição de carreira.Tenho conhecimento em linguagens como HTML, CSS
+  e JavaScript e uma paixão pela inovação tecnológica`,
+  pdf: '/assets/pdf/Lucas_Gomes.pdf',
+}
+
+// const linguagens = [
+//   {
+//     name: 'Html 5',
+//     color: {
+//       bg: 'bg-orange-600',
+//     },
+//   },
+//   {
+//     name: 'CSS 3',
+//     color: {
+//       bg: 'bg-blue-500',
+//     },
+//   },
+//   {
+//     name: 'Javascript',
+//     color: {
+//       bg: 'bg-yellow-400',
+//     },
+//   },
+//   {
+//     name: 'Taiwind Css',
+//     color: {
+//       bg: 'bg-blue-500',
+//     },
+//   },
+//   {
+//     name: 'React Js',
+//     color: {
+//       bg: 'bg-blue-500',
+//     },
+//   },
+//   {
+//     name: 'MongoDB',
+//     color: {
+//       bg: 'bg-green-400',
+//     },
+//   },
+//   {
+//     name: 'MySQL',
+//     color: {
+//       bg: '',
+//     },
+//   },
+//   {
+//     name: 'NextJS',
+//     color: {
+//       bg: '',
+//     },
+//   },
+//   {
+//     name: 'Bun',
+//     color: {
+//       bg: '',
+//     },
+//   },
+//   {
+//     name: 'Express',
+//     color: {
+//       bg: 'bg-gray-300',
+//     },
+//   },
+// ]
+export const projects = [
+  {
+    id: '01',
+    name: 'Portifolio 2.3',
+    description:
+      'Portifilio desenvolvido para treinamento de todas as tecnologias que venho aprendendo ate o momento.',
+    image: '/assets/images/projects/portifolio-2.3.png',
+    stack: 'back',
+    tecnologies: ['NextJS', 'Taiwind CSS', 'Typescript', 'MongoDB', 'Prisma'],
+    github: '',
+    page: 'https://web-lucasgomes-js.vercel.app/',
+  },
+  {
+    id: '02',
+    name: 'Jordan Shoes',
+    description:
+      'Este é 2º dos Desafios elaborados por Iuri Code , onde era preciso desenvolver um página em HTML5 e CSS3 de uma rede de calçados ,no entanto utilizei outras tecnologias a mais, com o intuito de praticar o que venho estudando atualmente.',
+    image: '/assets/images/projects/jordanshoes.png',
+    stack: 'front',
+    tecnologies: [
+      'Vite',
+      'Html5',
+      'CSS3',
+      'Javascript',
+      'ReactJS',
+      'Styled Components',
+    ],
+    github: 'https://github.com/llucasgomes/Jordan-Shoes',
+    page: 'https://jordan-shoes-react.vercel.app/',
+  },
+  {
+    id: '03',
+    name: 'Blog Codelândia',
+    description:
+      'Este é 1º dos Desafios elaborados por Iuri Code , onde era preciso desenvolver um página em HTML5 e CSS3  de um Blog ,no entanto utilizei outras tecnologias a mais, com o intuito de praticar o que venho estudando atualmente.',
+    image: '/assets/images/projects/blogcodelandia.png',
+    stack: 'front',
+    tecnologies: [
+      'Vite',
+      'Html5',
+      'CSS3',
+      'Javascript',
+      'ReactJS',
+      'Styled Components',
+    ],
+    github: 'https://github.com/llucasgomes/Blog-react',
+    page: 'https://blog-react-two.vercel.app/',
+  },
+  {
+    id: '04',
+    name: 'Mary Modas',
+    description:
+      'Site demonstrativo de Peças Intimas fêmeninas, contruido com o intuido de comecar a praticar Reactjs',
+    image: '/assets/images/projects/marymodas.png',
+    stack: 'front',
+    tecnologies: ['Vite', 'Html5', 'CSS3', 'Javascript', 'ReactJS'],
+    github: 'https://github.com/llucasgomes/mary-modas',
+    page: 'https://mary-modas.vercel.app/',
+  },
+  {
+    id: '05',
+    name: 'Lista de Alunos',
+    description:
+      'Esse é um Projeto onde faço uma SPA Tela de persons com filtragem, onde utilizo React e uma api publica, com o intuito de praticar o que venho estudando atualmente.',
+    image: '/assets/images/projects/listalunosapi.png',
+    stack: 'front',
+    tecnologies: [
+      'Vite',
+      'Html5',
+      'CSS3',
+      'Typescript',
+      'ReactJS',
+      'Styled Componets',
+    ],
+    github: 'https://github.com/llucasgomes/react-lista-persons-api',
+    page: 'https://react-lista-persons-api.vercel.app/',
+  },
+  {
+    id: '06',
+    name: 'To Do List',
+    description:
+      'Este projeto foi desenvolvido por mim para minha aula de conceitos iniciais em React,',
+    image: '',
+    stack: 'front',
+    tecnologies: [
+      'Vite',
+      'Html5',
+      'CSS3',
+      'Typescript',
+      'ReactJS',
+      'Styled Componets',
+    ],
+    github: 'https://github.com/llucasgomes/to-do-list-react',
+    page: '',
+  },
+  {
+    id: '07',
+    name: 'One Page',
+    description:
+      'Este é um dos Desafios elaborados por Iuri Code , onde era preciso desenvolver um página em HTML5 e CSS3 de uma Lading Page Simples ,no entanto utilizei ReactJS e Styled Components, com o intuito de praticar o que venho estudando atualmente.',
+    image: '/assets/images/projects/onepage.png',
+    stack: 'front',
+    tecnologies: [
+      'Vite',
+      'Html5',
+      'CSS3',
+      'Javascript',
+      'ReactJS',
+      'Styled Componets',
+    ],
+    github: 'https://github.com/llucasgomes/One-Page',
+    page: 'http://one-page-ecru.vercel.app/',
+  },
+  {
+    id: '08',
+    name: 'Desafio Stack X - VueJS',
+    description:
+      'Este é um dos 3  Desafio da Stack X elaborado pelo Mentor do modulo de WEB 3 Gabriel , onde era preciso desenvolver Lading Page Simples ,utilizando VueJs com TailwindCSS ,e enviar eventos pelos botões para trocar cada states (idioma).',
+    image: '/assets/images/projects/desafiostack.png',
+    stack: 'front',
+    tecnologies: ['Vite', 'Html5', 'CSS3', 'Javascript', 'Vue', 'Taiwind CSS'],
+    github: 'https://github.com/llucasgomes/Desafio-StackX-Web3-Vue',
+    page: 'http://desafio-stack-x-web3-vue.vercel.app/',
+  },
+  {
+    id: '09',
+    name: 'Desafio Stack X - ReacJS',
+    description:
+      'Este é um Desafio da Stack X elaborado pelo Mentor do modulo de WEB 3 Gabriel , onde era preciso desenvolver Lading Page Simples ,utilizando ReactJS + Styled Components com TypeScript ,e enviar eventos pelos botões para trocar cada states (idioma).',
+    image: '/assets/images/projects/desafiostack.png',
+    stack: 'front',
+    tecnologies: [
+      'Vite',
+      'Html5',
+      'CSS3',
+      'Typescript',
+      'ReacJS',
+      'Styled Components',
+    ],
+    github: 'https://github.com/llucasgomes/Desafio-StackX-Web3-React-Ts',
+    page: 'http://desafio-stack-x-web3-react-ts.vercel.app/',
+  },
+  {
+    id: '10',
+    name: 'Desafio Stack X - Angular',
+    description:
+      'Este é um dos Desafio da Stack X elaborado pelo Mentor do modulo de WEB 3 Gabriel , onde era preciso desenvolver Lading Page Simples ,utilizando Angular + SASS com TypeScript.',
+    image: '/assets/images/projects/desafiostack.png',
+    stack: 'front',
+    tecnologies: [
+      'Angular CLI',
+      'Html5',
+      'CSS3',
+      'Typescript',
+      'Angular',
+      'SASS',
+    ],
+    github: 'https://github.com/llucasgomes/Desafio-StackX-Web3-Angular-Ts',
+    page: 'http://desafio-stack-x-web3-angular-ts.vercel.app/',
+  },
+  {
+    id: '11',
+    name: 'Portifolio 2.2',
+    description:
+      'Portifilio desenvolvido para treinamento de todas as tecnologias que venho aprendendo ate o momento.',
+    image: '/assets/images/projects/portifolio-2.2.png',
+    stack: 'front',
+    tecnologies: ['Html5', 'CSS3', 'Javascript', 'ReactJs'],
+    github: 'https://github.com/llucasgomes/new-portiflio-reactjs',
+    page: 'https://new-portiflio-reactjs.vercel.app/',
+  },
+  {
+    id: '12',
+    name: 'Blog - Teste Técnico',
+    description:
+      'Este é um Teste Técnico de processo seletivo, onde é preciso desenvolver uma interface para exibir os posts de um blog e ao clicar no post os comentarios associados ,no entanto utilizei ReactJS e Styled Components.',
+    image: '/assets/images/projects/blog-challenge.png',
+    stack: 'front',
+    tecnologies: ['Html5', 'CSS3', 'Javascript', 'ReactJs'],
+    github: 'https://github.com/llucasgomes/blog-challenge',
+    page: 'https://blog-challenge-chi.vercel.app/',
+  },
+  {
+    id: '13',
+    name: 'Portifolio 2.0',
+    description:
+      'Portifilio desenvolvido para treinamento de todas as tecnologias que venho aprendendo ate o momento.',
+    image: '/assets/images/projects/portflio2.0.png',
+    stack: 'front',
+    tecnologies: ['Html5', 'CSS3', 'Typescript', 'Angular CLI'],
+    github: 'https://github.com/llucasgomes/portifolio-2-0',
+    page: 'https://portifolio-2-0-seven.vercel.app/',
+  },
+  {
+    id: '14',
+    name: 'Bun + Elysia + OpenAI',
+    description:
+      'Desenvolvimento de uma API integrada com o OpenAI, no entanto com as tecnologias recém (2023), com o intuito de aprender ',
+    image: '',
+    stack: 'back',
+    tecnologies: ['Bun', 'Elysia', 'OpenAI'],
+    github: 'https://github.com/llucasgomes/API-OpenAI-ElysiaJS',
+    page: '',
+  },
+  {
+    id: '15',
+    name: 'Marilza Cakes',
+    description: 'Projeto em desenvolvimento para fins de aprendizado',
+    image: '/assets/images/projects/marilzacakes.png',
+    stack: 'front',
+    tecnologies: ['NextJS', 'Typescript', 'Tailwind CSS'],
+    github: 'https://github.com/llucasgomes/API-OpenAI-ElysiaJS',
+    page: 'https://marilza-cakes.vercel.app/',
+  },
+  {
+    id: '16',
+    name: 'Biosante',
+    description: 'Site em que estou dando um upgrade, para fins de aprendizado',
+    image: '/assets/images/projects/biosante.png',
+    stack: 'front',
+    tecnologies: ['NextJS', 'Typescript', 'Tailwind CSS'],
+    github: 'https://github.com/llucasgomes/biosante-upgrade',
+    page: 'https://biosante-upgrade.vercel.app/',
+  },
+]
+
+export const courses = [
+  {
+    title: 'DESENVOLVIMENTO FULL STACK JR',
+    hours: 380,
+    description:
+      'Formação de profissionais qualificados no desenvolvimento de software Back/Front-End, através da capacitação de alto nível, atendendo as demandas da área e proporcionando acesso ao mercado de trabalho.',
+  },
+  {
+    title: 'DESENVOLVIMENTO FRONT-END',
+    hours: 80,
+    description:
+      'O curso vai muito além do básico, onde é ensinado conceitos avançados de programação fullstack, como React.js, Redux, Context API, TypeScript e Node.',
+  },
+]
+
+export const experiences = [
+  {
+    title: 'MONITOR MODULO FRONT-END',
+    description:
+      'Facilitando o treinamento focado no desenvolvimento front-end,respondendo perguntas, esclarecendo dúvidas sobre conceitos front-end e ajudadando/auxiliando os alunos com os desafios/exercicios práticos',
+    time: {
+      from: 2022,
+      to: '',
+    },
+  },
+]
+
+export const comments = [
+  {
+    id: '01',
+    name: 'Ghalba Vieira',
+    stack: 'Dev. de Front-end Pleno.',
+    image: 'ghalba-vieira.jpeg',
+    comment: `Durante o tempo em que lhe dei aula, fiquei impressionada com sua paixão
+    pela criação de experiências digitais incríveis e sua habilidade em
+    traduzir designs em códigos elegantes e funcionais. Ele possui ótimos
+    conhecimentos em frameworks modernos como ReactJS, e sempre busca
+    aprender novas tecnologias para se manter atualizado.`,
+  },
+  {
+    id: '02',
+    name: 'Maiara Santos',
+    stack: 'Aluna Dev. Full Stack Jr',
+    image: 'mayara.jpeg',
+    comment: `Estudamos na mesma Instituição de ensino e Lucas tem se destacado 
+    por ser um aluno disciplinado, presente em todas as atividades, além de exercer, 
+    um fundamento vasto em Linguagem de Programação Front- End. No Módulo Web 3.0, 
+    ele tem me orientado desde a construção até o término de uma aplicação executado em: 
+    Angular, react e Vue. Com o seu auxílio, tens proporcionado aprendizados essenciais 
+    para comigo. `,
+  },
+  {
+    id: '03',
+    name: 'Isa Santana',
+    stack: 'Coordenadora Acadêmica na StackX.',
+    image: 'isa.jpeg',
+    comment: `O Lucas Gomes foi convidado à ser monitor da StackX por se destacar
+     em todos os módulos do curso e sempre estar disposto em ajudar os demais alunos
+      com empatia fazendo sempre as entregas além do esperado.
+    Apresenta constante evolução, desenvolvendo projetos e está em permanente 
+    aprendizado aqui no curso.É muito gratificante tê-lo em nosso time de Monitores/Alunos.`,
+  },
+  {
+    id: '04',
+    name: 'Vivi Fernandes',
+    stack: 'Mentora | Marketing e Vendas Estratégicas',
+    image: 'vivi.jpeg',
+    comment: `O Lucas tem muito conhecimento e tem arrasado no Front-End.`,
+  },
+  {
+    id: '05',
+    name: 'Gabriel Lennon',
+    stack: 'Software Engineer.',
+    image: 'gabriel.jpeg',
+    comment: `Ótimo profissional, busca sempre estar em evolução e escrever soluções excelentes!`,
+  },
+  {
+    id: '06',
+    name: 'Maurilio Benevento',
+    stack: 'Mentor | Fundador Stack X & HANDS4E ',
+    image: 'maurilio.jpeg',
+    comment: `Sua atitude positiva, abertura a novas ideias e facilidade de trabalhar 
+    em equipe se somam às suas habilidades técnicas. O portfólio de Lucas reflete sua 
+    capacidade de transformar conceitos criativos em produtos funcionais e visualmente agradáveis.
+    Além disso, ele é  motivado e, certamente, continuará crescendo e 
+    se destacando na área de desenvolvimento.`,
+  },
+]
