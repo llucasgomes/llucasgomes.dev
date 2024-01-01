d66c7d0c3070d9266092ae63bb31f6fc02a4cc47
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2024-01-01 14:22:12 -0400

    :iphone: Notebook

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,13 +1,15 @@
 import { profile } from '@/lib/data'
+import { Download } from 'lucide-react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function page() {
   return (
-    <main className="flex h-[calc(100%+56px)] w-full flex-col items-center justify-center  py-14">
-      <h1 className="mb-5 mt-2 text-3xl">Sobre</h1>
+    <main className="flex h-[calc(100%+56px)] w-full flex-col items-center justify-center py-14">
+      <h1 className="mb-5 mt-2 text-3xl lg:hidden">Sobre</h1>
       <section
         className="flex h-3/5 w-10/12 flex-col items-center bg-[#373A3C]  
-      p-5 "
+      p-5 lg:flex-row"
       >
         <div className="w-full md:hidden">
           <Image
@@ -19,7 +21,10 @@ export default function page() {
           md:mb-0 md:h-[350px] md:w-full"
           />
         </div>
-        <div className="hidden w-full md:block">
+        <div
+          className="hidden  w-full md:block
+        "
+        >
           <Image
             src={'/assets/images/profile/perfil.webp'}
             width={350}
@@ -29,9 +34,11 @@ export default function page() {
           md:mb-0 md:h-[350px] md:w-full"
           />
         </div>
+
         <div
           className="flex flex-col justify-center 
-             md:items-center md:justify-center md:px-5"
+             md:items-center md:justify-center md:px-5 
+             lg:relative "
         >
           <h1 className="mb-2 text-3xl md:hidden">{profile.name}</h1>
           <h3 className="mb-2 text-2xl md:mt-5">Web Developer</h3>
@@ -51,6 +58,16 @@ export default function page() {
             comprovada em transformar conceitos criativos em produtos digitais
             funcionais e visualmente impressionantes.
           </p>
+          <Link
+            href={profile.pdf}
+            target="_blank"
+            download
+            className=" text-md absolute hidden  border-[1px]  border-[#F8F9FA] 
+                px-2 py-2  text-center text-[#F8FAFC] hover:bg-[#F8F9FA] hover:text-[#373A3C]
+                lg:right-0 lg:top-0 lg:block"
+          >
+            <Download />
+          </Link>
         </div>
       </section>
     </main>
