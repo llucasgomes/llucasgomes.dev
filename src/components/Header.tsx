2490d34414dc18ce28b0e454bb5666c0810826de
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2024-01-01 10:27:26 -0400

    :bug: fix:  ButtonMenu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,11 @@
+import Link from 'next/link'
 import ButtonMenu from './ButtonMenu'
-import MenuMobile from './MenuMobile'
 
 export default function Header() {
   return (
     <header className=" flex h-20 w-full  items-center justify-around bg-black">
       <h1 className='text-2xl font-bold'>Lucas Gomes</h1>
-      {/* <nav className="hidden md:block">
+      <nav className="hidden md:block">
         <ul className="flex w-full list-none gap-5">
           <Link href={'/'} className="cursor-pointer text-white">
             Home
@@ -20,12 +20,11 @@ export default function Header() {
             Contato
           </Link>
         </ul>
-      </nav> */}
+      </nav>
+        <div className='md:hidden'>
         <ButtonMenu />
-        <h2>ola mundo</h2>
-        <MenuMobile />
-      {/* <div className="block md:hidden">
-      </div> */}
+        </div>
+   
     </header>
   )
 }
