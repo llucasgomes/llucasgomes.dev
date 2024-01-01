d1508b645f98bbd5f89cec003f64c6305d64d8fa
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2024-01-01 11:14:27 -0400

    :test_tube: test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
-import Comment from "@/components/Comment";
 import Hero from "@/components/Hero";
-import Tecnologies from "@/components/Tecnologies";
 
 export default function Home() {
   return (
     <main className=" flex w-full flex-col items-center justify-center ">
       <Hero />
-      <Tecnologies />
-      <Comment />
+      {/* <Tecnologies />
+      <Comment /> */}
 
       {/* <div className="flex w-full  flex-col gap-7 pt-14 md:w-[60%]">
         <HistoryDev />
