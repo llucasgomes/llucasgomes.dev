14d8268e4c08498215d35c3ae7d7fa3633139fbc
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2024-01-01 10:04:54 -0400

    bug:teste 04

---
+++ src/components/HeaderTeste.tsx
@@ -0,0 +1,16 @@
+import MenuMobile from './MenuMobile'
+
+export default function HeaderTeste() {
+  return (
+    <header className="flex  h-20 w-full items-center justify-around bg-[#D9D9D9]">
+      {/* <Image
+        src={'/image/logoWebLarge-1.webp'}
+        width={176}
+        height={51}
+        alt="Logo Domus Petra"
+      /> */}
+
+      <MenuMobile />
+    </header>
+  )
+}
