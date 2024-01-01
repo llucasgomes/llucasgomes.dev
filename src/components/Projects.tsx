d0932bc730b3b102264a508d118a15a48041b04c
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2024-01-01 08:50:36 -0400

    :sparkles: feat: Section by "Projects"

---
+++ src/components/Projects.tsx
@@ -0,0 +1,51 @@
+import { projects } from '@/lib/data'
+import CardProject from './CardProject'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from './ui/tabs'
+
+export default function Projects() {
+  const projectBack = projects.filter((project) => project.stack === 'back')
+  const projectFront = projects.filter((project) => project.stack === 'front')
+
+  return (
+    <section className=" flex w-full flex-col items-center justify-start  py-14">
+      <h1 className="mb-5 text-3xl">Projetos</h1>
+      <h2 className="mb-3 max-w-5xl px-5 text-base">
+        Projetos realizados durante minha evolução como Desenvolvedor Jr
+      </h2>
+
+      <div className="flex  w-full max-w-5xl flex-wrap justify-center  px-5">
+        <Tabs defaultValue="all" className="w-full max-w-5xl">
+          <TabsList className="flex w-full items-center justify-center">
+            <TabsTrigger value="all">All</TabsTrigger>
+            <TabsTrigger value="front">Front-End</TabsTrigger>
+            <TabsTrigger value="back">Back-End</TabsTrigger>
+          </TabsList>
+          <TabsContent
+            value="all"
+            className="flex  w-full max-w-5xl flex-wrap justify-center gap-6"
+          >
+            {projects.map((project) => {
+              return <CardProject key={project.id} project={project} />
+            })}
+          </TabsContent>
+          <TabsContent
+            value="front"
+            className="flex  w-full max-w-5xl flex-wrap justify-center gap-6"
+          >
+            {projectFront.map((project) => {
+              return <CardProject key={project.id} project={project} />
+            })}
+          </TabsContent>
+          <TabsContent
+            value="back"
+            className="flex  w-full max-w-5xl flex-wrap justify-center gap-6"
+          >
+            {projectBack.map((project) => {
+              return <CardProject key={project.id} project={project} />
+            })}
+          </TabsContent>
+        </Tabs>
+      </div>
+    </section>
+  )
+}
