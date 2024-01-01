3270e10e0056decb1edd60a31a4f23dcc48f3e86
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2024-01-01 12:33:52 -0400

    :sparkles: chore: Add filter "FullStack"

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -5,9 +5,10 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from './ui/tabs'
 export default function Projects() {
   const projectBack = projects.filter((project) => project.stack === 'back')
   const projectFront = projects.filter((project) => project.stack === 'front')
+  const projectFullStack = projects.filter((project) => project.stack === 'fullstack')
 
   return (
-    <section className=" flex w-full flex-col items-center justify-start  py-14">
+    <section className=" flex w-full flex-col items-center justify-start  pb-14">
       <h1 className="mb-5 text-3xl">Projetos</h1>
       <h2 className="mb-3 max-w-5xl px-5 text-base">
         Projetos realizados durante minha evolução como Desenvolvedor Jr
@@ -19,6 +20,7 @@ export default function Projects() {
             <TabsTrigger value="all">All</TabsTrigger>
             <TabsTrigger value="front">Front-End</TabsTrigger>
             <TabsTrigger value="back">Back-End</TabsTrigger>
+            <TabsTrigger value="fullstack">Full Stack</TabsTrigger>
           </TabsList>
           <TabsContent
             value="all"
@@ -33,6 +35,14 @@ export default function Projects() {
             className="flex  w-full max-w-5xl flex-wrap justify-center gap-6"
           >
             {projectFront.map((project) => {
+              return <CardProject key={project.id} project={project} />
+            })}
+          </TabsContent>
+          <TabsContent
+            value="fullstack"
+            className="flex  w-full max-w-5xl flex-wrap justify-center gap-6"
+          >
+            {projectFullStack.map((project) => {
               return <CardProject key={project.id} project={project} />
             })}
           </TabsContent>
