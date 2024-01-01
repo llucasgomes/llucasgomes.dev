565cda9f9ad4abce8c72fe84c89f4f09e7342efa
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2024-01-01 10:08:34 -0400

    bug:teste 05

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,15 @@
-import Comment from '@/components/Comment'
-import Courses from '@/components/Courses'
-import Hero from '@/components/Hero'
-import HistoryDev from '@/components/HistoryDev'
-import Tecnologies from '@/components/Tecnologies'
 
 export default function Home() {
   return (
     <main className=" flex w-full flex-col items-center justify-center ">
-      <Hero />
+      {/* <Hero />
       <Tecnologies />
       <Comment />
 
       <div className="flex w-full  flex-col gap-7 pt-14 md:w-[60%]">
         <HistoryDev />
         <Courses />
-      </div>
+      </div> */}
       {/* <Projects /> */}
     </main>
   )
--- src/components/HeaderTeste.tsx
+++ src/components/HeaderTeste.tsx
@@ -2,7 +2,7 @@ import MenuMobile from './MenuMobile'
 
 export default function HeaderTeste() {
   return (
-    <header className="flex  h-20 w-full items-center justify-around bg-[#D9D9D9]">
+    <header className="flex h-20 w-full items-center justify-around bg-[#D9D9D9]">
       {/* <Image
         src={'/image/logoWebLarge-1.webp'}
         width={176}
