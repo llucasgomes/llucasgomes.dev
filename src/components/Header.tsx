081145122719e6ef27e64fc87f7a19857edbb8f7
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2024-01-01 10:00:47 -0400

    bug:teste 03

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,10 @@
-import Link from 'next/link'
 import ButtonMenu from './ButtonMenu'
 
 export default function Header() {
   return (
-    <header className=" flex h-14 w-screen  items-center justify-around bg-black px-3">
+    <header className=" flex h-20 w-screen  items-center justify-around bg-black">
       <h1 className='text-2xl font-bold'>Lucas Gomes</h1>
-      <nav className="hidden md:block">
+      {/* <nav className="hidden md:block">
         <ul className="flex w-full list-none gap-5">
           <Link href={'/'} className="cursor-pointer text-white">
             Home
@@ -20,10 +19,10 @@ export default function Header() {
             Contato
           </Link>
         </ul>
-      </nav>
-      <div className="  block md:hidden">
+      </nav> */}
         <ButtonMenu />
-      </div>
+      {/* <div className="block md:hidden">
+      </div> */}
     </header>
   )
 }
