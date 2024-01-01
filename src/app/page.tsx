21dd4be372c5a89a730b76b304adc72f0a0c2844
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2024-01-01 13:02:16 -0400

    chore: Add again "History Dev" and "Courses"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,25 @@
 import Comment from "@/components/Comment";
+import Courses from "@/components/Courses";
 import Hero from "@/components/Hero";
+import HistoryDev from "@/components/HistoryDev";
 import Tecnologies from "@/components/Tecnologies";
+import { Separator } from "@/components/ui/separator";
 
 export default function Home() {
   return (
     <main className=" flex w-full flex-col items-center justify-center ">
       <Hero />
-      <Comment />
+      <Separator/>
       <Tecnologies />
-
-      {/* <div className="flex w-full  flex-col gap-7 pt-14 md:w-[60%]">
+      <Separator/>
+      <Comment />
+      <Separator/>
+      <div className="flex w-full  flex-col gap-7 pt-14 
+       md:flex-row">
         <HistoryDev />
         <Courses />
-      </div> */}
-      {/* <Projects /> */}
+      </div> 
+      
     </main>
   )
 }
