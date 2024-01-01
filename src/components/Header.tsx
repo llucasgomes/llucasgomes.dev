430767fcbf6232c5f7b8ff6aaf9bdb62220b7bfb
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2024-01-01 09:37:14 -0400

    chore: position button menu to right

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,8 @@ import ButtonMenu from './ButtonMenu'
 
 export default function Header() {
   return (
-    <header className="fixed z-50 flex h-14 w-full  items-center justify-center bg-black">
+    <header className="fixed z-50 flex h-14 w-full  items-center justify-around bg-black px-3">
+      <h1 className='text-2xl font-bold'>Lucas Gomes</h1>
       <nav className="hidden md:block">
         <ul className="flex w-full list-none gap-5">
           <Link href={'/'} className="cursor-pointer text-white">
@@ -20,7 +21,7 @@ export default function Header() {
           </Link>
         </ul>
       </nav>
-      <div className="block md:hidden">
+      <div className="  block md:hidden">
         <ButtonMenu />
       </div>
     </header>
